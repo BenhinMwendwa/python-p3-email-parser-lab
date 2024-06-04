a6b6cb4996976c4c73632d87b55daa28329ee7ab
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-06-04 15:01:27 +0300

    email

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,12 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+    
+        addresses = re.split(r'\s*,\s*', self.email_addresses)
+        return addresses
+
